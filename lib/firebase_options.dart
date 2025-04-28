b30051713c3fe00d7ecb8d06ce20ca74b93222de
WilsonOpudo <wilsonopudo8@gmail.com>
2025-04-27 19:36:05 -0500

    Update firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,13 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 
 class DefaultFirebaseOptions {
-  static FirebaseOptions get web => const FirebaseOptions(
-        apiKey: "AIzaSyASwnVymTn6Ve6bjzncxzgnucxcQ1uagkk",
-        authDomain: "meetme-cf7ff.firebaseapp.com",
-        projectId: "meetme-cf7ff",
-        storageBucket: "meetme-cf7ff.appspot.com",
-        messagingSenderId: "1003468096156",
-        appId: "1:1003468096156:web:f1cc8430eb902cee1b6359",
-        measurementId: "G-E4LNLE7PPB",
-      );
+
 }
